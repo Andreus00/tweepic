13c204661c539bed94d92ac296b5f8199fda4d9b
andrea <sanchietti.1883210@studenti.uniroma1.it>
2023-06-01 13:23:52 +0200

    Added some code for the tweet stream

---
+++ src/tweet_stream/tweet_stream.py
@@ -0,0 +1,31 @@
+import tweepy
+import json
+import datetime
+import twint
+
+
+class TweetRetriever:
+
+    def __init__(self) -> None:
+        with open("data/token.json") as f:
+            self.token = json.load(f)
+        self.api = tweepy.Client(**self.token)
+
+    def tweet_to_list(self, tweet):
+        return [tweet.id, tweet.created_at.year, tweet.created_at.month, tweet.created_at.day, tweet.text]
+
+    def get_tweet(self, id):
+        return self.tweet_to_list(self.api.get_tweet(id=id, tweet_fields=["created_at", "text"]).data)
+    
+    def search(self, query, count=10, toDate=datetime.datetime.now() - datetime.timedelta(seconds=9), tweet_fields=["created_at", "text"]):
+        print(toDate)
+        result = self.api.search_recent_tweets(query=query, max_results=count, end_time=toDate, tweet_fields=tweet_fields)
+        return [self.tweet_to_list(tweet) for tweet in result.data]
+        
+
+if __name__ == "__main__":
+
+    twert_ret = TweetRetriever()
+    tweets = twert_ret.search("python", count=10)
+    print(tweets)
+    
